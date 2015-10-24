8c96d0bbf7a8a5dcb656b07e29eab375d34685ce
Yu-Hsin Hung <hungys@hotmail.com>
2015-10-24 22:35:21 +0800

    Support add start in Client CLI

--- client/command/app.py
+++ client/command/app.py
@@ -1,5 +1,6 @@
 import json
 import urllib2
+import time
 import config
 
 def app_list(args):
@@ -18,6 +19,29 @@ def app_list(args):
     except:
         print "Error: unexpected error"
 
+def app_start(args):
+    while True:
+        try:
+            resp = json.loads(urllib2.urlopen(config.server_url + "/api/app/" + args[0] + "/workunit").read())
+            if resp["workunit_id"] != "-1":
+                print "Get a new work!"
+                print "Workunit ID: " + resp["workunit_id"]
+                print "Input ID: " + resp["input_id"]
+                time.sleep(2)
+            else:
+                break
+        except urllib2.HTTPError as e:
+            if e.code == 404:
+                print "[Error] app not found"
+            else:
+                print "[Error] status code = " + str(e.code)
+        except KeyboardInterrupt:
+            break
+        except:
+            print "Error: unexpected error"
+
 def execute(args):
     if args[1] == "list":
         app_list(args[2:])
+    elif args[1] == "start":
+        app_start(args[2:])
